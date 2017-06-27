98730a3342d6dbac64266d39255722c7e713cf92
Apoorv <patelapoorv9990@gmail.com>
2017-06-26 19:40:13 -0700

    added login layout

--- app/src/main/java/com/moby/apply/activities/LoginActivity.java
+++ app/src/main/java/com/moby/apply/activities/LoginActivity.java
@@ -2,14 +2,34 @@ package com.moby.apply.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.moby.apply.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.edit_email)
+    EditText emailEditText;
+
+    @BindView(R.id.edit_password)
+    EditText passwordEditText;
+
+    @BindView(R.id.button_login)
+    Button loginButton;
+
+    @BindView(R.id.button_register)
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        
     }
 }
